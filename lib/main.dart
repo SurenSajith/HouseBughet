e92baa712909d16c521ad58c9021f77d1d05e6b3
Manodya Sathsarani Abeysinghe Ransitige <manodyasathsaraniabeysingheransitige@Manodyas-Mac-mini.local>
2026-04-03 14:39:30 +0530

    Initial Phase

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void Main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
+s
